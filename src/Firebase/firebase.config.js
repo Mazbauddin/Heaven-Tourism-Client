c8b28f2844db58081d3d33cec71d71e47b0ebf92
Mazbauddin <mazbauddin2010@gmail.com>
2024-04-29 17:15:02 +0600

    updated firebase config

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC-HP0uptfJnEtBZ631Z6hntg12LH9fg04",
-  authDomain: "heaven-tourism-management-site.firebaseapp.com",
-  projectId: "heaven-tourism-management-site",
-  storageBucket: "heaven-tourism-management-site.appspot.com",
-  messagingSenderId: "1037209766292",
-  appId: "1:1037209766292:web:2c8bd2b177a991979ae781",
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
